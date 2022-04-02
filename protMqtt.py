09157f53cbf6530e318045b280718ff99a565e6c
Izabela Marina <83678692+IzabelaMarina@users.noreply.github.com>
2022-04-01 23:25:18 -0300

    v3
    Conectado na bancada 5, imagens de fundo finais, gerando gráficos

--- protMqtt.py
+++ protMqtt.py
@@ -1,8 +1,8 @@
 import paho.mqtt.client as mqtt
 
 #Credenciais
-user = "grupo1-bancadaA8"
-passwd = "L@Bdygy1A8"
+user = "grupo1-bancadaA5"
+passwd = "L@Bdygy1A5"
 Broker = "labdigi.wiseful.com.br"           
 Port = 80                           
 KeepAlive = 60
@@ -10,24 +10,47 @@ KeepAlive = 60
 # Quando conectar na rede (Callback de conexao)
 def on_connect(client, userdata, flags, rc):
   print("Conectado com codigo " + str(rc))
-  client.subscribe(user+"/E0", qos=0)
-  print("Conectado com codigo E0")
-  client.subscribe(user+"/E1", qos=0)
+  client.subscribe(user+"/S0", qos=0)
+  client.subscribe(user+"/S1", qos=0)
+  client.subscribe(user+"/S2", qos=0)
+  client.subscribe(user+"/S3", qos=0)
+  client.subscribe(user+"/S4", qos=0)
+  client.subscribe(user+"/S5", qos=0)
 
 # Quando receber uma mensagem (Callback de mensagem)
 def on_message(client, userdata, msg):
   print(str(msg.topic)+" "+str(msg.payload.decode("utf-8")))
-  global pronto
-  pronto=0
-  if str(msg.topic) == user+"/E0":
-    print("Recebi uma mensagem de E0")
-    pronto=1
-  elif str(msg.topic) == user+"/E1":
-    print("Recebi uma mensagem de E1")
+  global ganhou
+  global perdeu
+  global rodada
+  global audio
+  ganhou=0
+  perdeu=0
+  rodada=[0,0,0]
+  audio= 0
+  if str(msg.topic) == user+"/S5":
+    print("Recebi uma mensagem de S5")
+    if str(msg.payload.decode("utf-8")) == '1':
+      ganhou=1
+  elif str(msg.topic) == user+"/S4":
+    print("Recebi uma mensagem de S4")
+    if str(msg.payload.decode("utf-8")) == '1':
+      perdeu=1
+  elif str(msg.topic) == user+"/S0":
+    rodada[0]= msg.payload.decode("utf-8")
+  elif str(msg.topic) == user+"/S1":
+    rodada[1]= msg.payload.decode("utf-8")
+  elif str(msg.topic) == user+"/S2":
+    rodada[2]= msg.payload.decode("utf-8")
+  elif str(msg.topic) == user+"/S3":
+    if str(msg.payload.decode("utf-8")) == '1':
+      audio=1
   else:
     print("Erro! Mensagem recebida de tópico estranho")
 
-pronto=0
+perdeu=0
+ganhou=0
+audio=0
 client = mqtt.Client()              
 client.on_connect = on_connect      
 client.on_message = on_message  
--- turinginverso.py
+++ turinginverso.py
@@ -5,62 +5,93 @@ import sys
 import time
 from pygame import display, event, font 
 from pygame.image import load
+from playsound import playsound
 
+#Botão circular
 class Button:
-    def __init__(self, xpos, ypos, radius, color_light, color_dark,text):
+    def __init__(self, xpos, ypos, sizex,sizey, color_light, color_dark,text,circle):
         self.xpos = xpos
         self.ypos = ypos
-        self.radius = radius
+        self.sizex = sizex
+        self.sizey = sizey
         self.color_light = color_light
         self.color_dark = color_dark
         self.text = text
+        self.circle = circle
 
     def putText(self,tela):
-        tela.blit(self.text,(self.xpos-12,self.ypos-15))
-
-    def isOver(self,mouse):
-        if math.sqrt((self.xpos-mouse[0])**2+(self.ypos-mouse[1])**2)<self.radius:
-            return True
+        if self.circle:
+            tela.blit(self.text,(self.xpos-12,self.ypos-15))
         else:
-            return False
+            tela.blit(self.text,(self.xpos+8,self.ypos+15))
+    #Para botões circulares sizex=sizey=raio
+    def isOver(self,mouse):
+        if self.circle:
+            if math.sqrt((self.xpos-mouse[0])**2+(self.ypos-mouse[1])**2)<self.sizex:
+                return True
+            else:
+                return False
+        else:
+            if (self.xpos<mouse[0]<self.xpos+self.sizex and self.ypos<mouse[1]<self.ypos+self.sizey):
+                return True
+            else:
+                return False
     def drawLight(self,tela):
-        pg.draw.circle(tela,self.color_light,(self.xpos,self.ypos),self.radius)
+        if self.circle:
+            pg.draw.circle(tela,self.color_light,(self.xpos,self.ypos),self.sizex)
+        else:
+            pg.draw.rect(tela,self.color_light,[self.xpos,self.ypos,self.sizex,self.sizey])
     def drawDark(self,tela):
-        pg.draw.circle(tela,self.color_dark,(self.xpos,self.ypos),self.radius)
+        if self.circle:
+            pg.draw.circle(tela,self.color_dark,(self.xpos,self.ypos),self.sizex)
+        else:
+            pg.draw.rect(tela,self.color_dark,[self.xpos,self.ypos,self.sizex,self.sizey])
+
+
 
 pg.init()
 
 #Cria tela
-width = 300
-height =500
+width = 640
+height =480
 tela = display.set_mode(size=(width, height))
 display.set_caption("Turing Inverso")
 
 #Imagem de fundo
+fundoInicio = load('imagens/1.png').convert()
+fundoInst = load('imagens/2.png').convert()
 fundoJogo = load('imagens/fundo.png').convert()
+fundoFinal = load('imagens/fim.png').convert()
 
 telaInicial=True
 telaInst=False
 telaJogo=False
-telaRes=False
+telaWin=False
+telaLos=False
 
-font = pg.font.SysFont('Changa-VariableFont_wght',20)
+font = pg.font.SysFont('Changa-VariableFont_wght',50)
+JFont = pg.font.SysFont('Changa-VariableFont_wght',30)
+RFont = pg.font.SysFont('Changa-VariableFont_wght',22)
 
 #Botões
-textJog = font.render('Jogar',True,(0,0,0))
-ButtonJ = Button(width/2,4*height/5,30,(210,90,90),(210,57,57),textJog)
+textJog = JFont.render('Iniciar',True,(0,0,0))
+ButtonJ = Button(width/2-40,6*height/7,80,50,(210,90,90),(210,57,57),textJog,False)
+
+textRein = RFont.render('Reiniciar',True,(0,0,0))
+ButtonR = Button(width/2-40,4*height/5+10,80,50,(210,90,90),(210,57,57),textRein,False)
 
 textA = font.render('A',True,(0,0,0))
-ButtonA = Button(width/4,2*height/5,30,(210,90,90),(210,57,57),textA)
+ButtonA = Button(width/5,6*height/7,30,30,(210,90,90),(210,57,57),textA,True)
 
 textB = font.render('B',True,(0,0,0))
-ButtonB = Button(3*width/4,2*height/5,30,(210,90,90),(210,57,57),textB)
+ButtonB = Button(2*width/5,6*height/7,30,30,(210,90,90),(210,57,57),textB,True)
 
 textC = font.render('C',True,(0,0,0))
-ButtonC = Button(width/4,3*height/5,30,(210,90,90),(210,57,57),textC)
+ButtonC = Button(3*width/5,6*height/7,30,30,(210,90,90),(210,57,57),textC,True)
 
 textD = font.render('D',True,(0,0,0))
-ButtonD = Button(3*width/4,3*height/5,30,(210,90,90),(210,57,57),textD)
+ButtonD = Button(4*width/5,6*height/7,30,30,(210,90,90),(210,57,57),textD,True)
+
 
 
 mqtt.client.connect(mqtt.Broker,mqtt.Port,mqtt.KeepAlive)
@@ -73,7 +104,26 @@ while(not end):
     mouse = pg.mouse.get_pos()
 
     if telaInicial:
-        tela.fill((0,0,0))
+        tela.blit(fundoInicio,(0,0))
+        if ButtonJ.isOver(mouse):
+            ButtonJ.drawLight(tela)
+        else:
+            ButtonJ.drawDark(tela)
+
+        ButtonJ.putText(tela)
+
+        pg.display.update()
+
+        for event in pg.event.get():
+                if(event.type == pg.QUIT):
+                    pg.quit() 
+                if (event.type == pg.MOUSEBUTTONDOWN):  
+                    if ButtonJ.isOver(mouse):
+                        telaInst=True
+                        telaInicial=False
+    
+    elif telaInst:
+        tela.blit(fundoInst,(0,0))
 
         if ButtonJ.isOver(mouse):
             ButtonJ.drawLight(tela)
@@ -89,10 +139,13 @@ while(not end):
                     pg.quit() 
                 if (event.type == pg.MOUSEBUTTONDOWN):  
                     if ButtonJ.isOver(mouse):
+                        mqtt.client.publish(mqtt.user+"/E2", payload="1", qos=0, retain=False)
+                        time.sleep(0.6)
+                        mqtt.client.publish(mqtt.user+"/E2", payload="0", qos=0, retain=False)
                         telaJogo=True
-                        telaInicial=False
+                        telaInst=False
 
-    if telaJogo:
+    elif telaJogo:
 
         tela.blit(fundoJogo,(0,0))
 
@@ -123,30 +176,96 @@ while(not end):
         
         pg.display.update()
 
+        if mqtt.audio==1:
+            playsound('beep-01a.mp3')
+            time.sleep(0.1)
+            mqtt.client.publish(mqtt.user+"/S6", payload="1", qos=0, retain=False)
+            time.sleep(0.6)
+            mqtt.client.publish(mqtt.user+"/S6", payload="0", qos=0, retain=False)
+
 
         for event in pg.event.get():
             if(event.type == pg.QUIT):
                 pg.quit() 
             if (event.type == pg.MOUSEBUTTONDOWN):  
                 if ButtonA.isOver(mouse):
-                    mqtt.client.publish(mqtt.user+"/S0", payload="1", qos=0, retain=False)
+                    mqtt.client.publish(mqtt.user+"/E3", payload="1", qos=0, retain=False)
                     time.sleep(0.6)
-                    mqtt.client.publish(mqtt.user+"/S0", payload="0", qos=0, retain=False)
+                    mqtt.client.publish(mqtt.user+"/E3", payload="0", qos=0, retain=False)
                 elif ButtonB.isOver(mouse):
-                    mqtt.client.publish(mqtt.user+"/S0", payload="1", qos=0, retain=False)
+                    mqtt.client.publish(mqtt.user+"/E4", payload="1", qos=0, retain=False)
                     time.sleep(0.6)
-                    mqtt.client.publish(mqtt.user+"/S0", payload="0", qos=0, retain=False)
+                    mqtt.client.publish(mqtt.user+"/E4", payload="0", qos=0, retain=False)
                 elif ButtonC.isOver(mouse):
-                    mqtt.client.publish(mqtt.user+"/S0", payload="1", qos=0, retain=False)
+                    mqtt.client.publish(mqtt.user+"/E5", payload="1", qos=0, retain=False)
                     time.sleep(0.6)
-                    mqtt.client.publish(mqtt.user+"/S0", payload="0", qos=0, retain=False)
+                    mqtt.client.publish(mqtt.user+"/E5", payload="0", qos=0, retain=False)
                 elif ButtonD.isOver(mouse):
-                    mqtt.client.publish(mqtt.user+"/S0", payload="1", qos=0, retain=False)
+                    mqtt.client.publish(mqtt.user+"/E6", payload="1", qos=0, retain=False)
                     time.sleep(0.6)
-                    mqtt.client.publish(mqtt.user+"/S0", payload="0", qos=0, retain=False)
+                    mqtt.client.publish(mqtt.user+"/E6", payload="0", qos=0, retain=False)
 
-        if mqtt.pronto == 1:
-            end=True
+        if mqtt.ganhou == 1:
+            telaWin = True
+            telaJogo= False
+            mqtt.ganhou = 0
+
+        if mqtt.perdeu == 1:
+            telaLos = True
+            telaJogo = False
+            mqtt.perdeu = 0
+
+    elif telaWin:
+        tela.blit(fundoFinal,(0,0))
+
+        vitorias=load('imagens/graphVitorias.png').convert()
+        rod=load('imagens/graphRodadas.png').convert()
+
+        tela.blit(vitorias,(80,180))
+        tela.blit(rod,(350,180))
+        
+        if ButtonR.isOver(mouse):
+            ButtonR.drawLight(tela)
+        else:
+            ButtonR.drawDark(tela)
+
+        ButtonR.putText(tela)
+
+        pg.display.update()
+
+        for event in pg.event.get():
+                if(event.type == pg.QUIT):
+                    pg.quit() 
+                if (event.type == pg.MOUSEBUTTONDOWN):  
+                    if ButtonR.isOver(mouse):
+                        telaJogo=True
+                        telaWin=False
+
+    elif telaLos:
+        tela.blit(fundoFinal,(0,0))
+
+        vitorias=load('imagens/graphVitorias.png').convert()
+        rod=load('imagens/graphRodadas.png').convert()
+
+        tela.blit(vitorias,(80,180))
+        tela.blit(rod,(350,180))
+
+        if ButtonR.isOver(mouse):
+            ButtonR.drawLight(tela)
+        else:
+            ButtonR.drawDark(tela)
+
+        ButtonR.putText(tela)
+
+        pg.display.update()
+
+        for event in pg.event.get():
+                if(event.type == pg.QUIT):
+                    pg.quit() 
+                if (event.type == pg.MOUSEBUTTONDOWN):  
+                    if ButtonR.isOver(mouse):
+                        telaJogo=True
+                        telaLos=False
             
 mqtt.client.loop_stop()
 
